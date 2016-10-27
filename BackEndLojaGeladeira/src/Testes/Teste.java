a857bb15a7d1b9bf51839c3b83f47af8deeefc2c
Gabriel Rado <gabriel119435@gmail.com>
2016-10-27 15:16:29 -0200

    nome de classes e pacotes

---
+++ BackEndLojaGeladeira/src/Testes/Teste.java
@@ -0,0 +1,14 @@
+package Testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Teste {
+
+	@Test
+	public void test_metodoFachada() {
+		fail("Not yet implemented");
+	}
+
+}
