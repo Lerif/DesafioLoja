a7d7c8fa8b3fd3b6d8d50dff3ee96bacfd819fea
Lerif <ellogador@gmail.com>
2016-10-28 02:41:44 -0200

    testes da fachada com servico geladeira OK !

--- BackEndLojaGeladeira/src/Fachada/Fachada.java
+++ BackEndLojaGeladeira/src/Fachada/Fachada.java
@@ -2,6 +2,7 @@ package Fachada;
 
 import java.util.List;
 
+import Entidades.Caracteristica;
 import Entidades.Geladeira;
 import Servicos.ServicoGeladeira;
 import Servicos.ServicoLoja;
@@ -11,12 +12,23 @@ public class Fachada {
 	public ServicoGeladeira servicoGeladeira = ServicoGeladeira.novo();
 	public ServicoLoja servicoLoja = ServicoLoja.novo();
 	
-	public Geladeira criarNovaGeladeira(String nome, String marca, String fabricante, List<String> fotos){
-		return servicoGeladeira.criarGeladeira(nome, marca, fabricante, fotos);
+	public Geladeira criarNovaGeladeira(String nome, String marca, String fabricante,
+			 List<Caracteristica> caracteristicas, List<String> fotos){
+		return servicoGeladeira.criarGeladeira(nome, marca, fabricante, caracteristicas, fotos);
 	}
 	
 	public List<Geladeira> buscarGeladeira(String nomeDaGeladeira){
 		return servicoGeladeira.buscarGeladeira(nomeDaGeladeira);
 	}
+	
+	public List<Geladeira> buscarTodasGeladeiras(){
+		return servicoGeladeira.buscarTodasGelardeiras();
+	}
+	
+	public boolean excluirGeladeira(Geladeira geladeira){
+		return servicoGeladeira.excluirGeladeira(geladeira);
+	}
+	
+	
 	
 }
